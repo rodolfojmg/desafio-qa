1f3f125d587124c0be4228d9b35635c71070266f
Rodolfo Gonçalves <rodolfo.goncalves@concrete.com.br>
2019-06-01 12:14:20 -0300

    adicionando page objects

--- quality_assurance/features/page_objects/list_imports.js
+++ quality_assurance/features/page_objects/list_imports.js
@@ -1,9 +1,14 @@
 'use strict'
-var Helper = require('../shared_libs/helper.js')
+// var Helper = require('../shared_libs/helper.js')
 
 class ListImports {
   constructor () {
-    this.helper = new Helper()
+    // this.helper = new Helper()
+
+  }
+
+  openPage () {
+    browser.get('https://desafio-qa.herokuapp.com/')
   }
 }
 
